15283fed5ab59d4ecf3dafc29deb4503b87669ea
Alec Taylor <alec.taylor79@gmail.com>
2017-06-29 22:30:16 +0100

    add redux logger

--- client/index.js
+++ client/index.js
@@ -3,14 +3,17 @@ import { BrowserRouter, Route, browserHistory, Switch, IndexRoute } from 'react-
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import { loadTodos } from './actions';
 import todoApp from './reducers';
 import App from './components/App';
 
+const logger = createLogger();
+
 const store = createStore(
   todoApp,
-  compose(applyMiddleware(thunk))
+  compose(applyMiddleware(thunk, logger))
 );
 
 render(
